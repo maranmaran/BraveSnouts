5501442bc56bb64b8fe33ea6ce4127b5212a2653
Marko Urh <urh.marko@gmail.com>
2021-02-12 14:42:44 +0100

    refactor: Best bid on image gallery

--- brave-snouts-frontend/src/app/features/auction-feature/item/item-gallery/item-gallery.component.ts
+++ brave-snouts-frontend/src/app/features/auction-feature/item/item-gallery/item-gallery.component.ts
@@ -1,8 +1,11 @@
-import { Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { SingleItemDialogComponent } from 'src/app/features/auction-feature/item/single-item-dialog/single-item-dialog.component';
 import { AuctionItem } from 'src/business/models/auction-item.model';
+import { AuthService } from 'src/business/services/auth.service';
 import { ItemDialogService } from 'src/business/services/item-dialog.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-item-gallery',
@@ -10,22 +13,38 @@ import { ItemDialogService } from 'src/business/services/item-dialog.service';
   styleUrls: ['./item-gallery.component.scss'],
   providers: [ItemDialogService]
 })
-export class ItemGalleryComponent implements OnInit, OnChanges {
+export class ItemGalleryComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() items: AuctionItem[];
   @Input() parentScroll: ElementRef;
   @Output() loadMore = new EventEmitter<any>();
 
+  userId: string;
+  isAuthenticated$: Observable<boolean>;
+
+  private readonly _subsink = new SubSink();
+
   constructor(
     private readonly dialog: MatDialog,
-    private readonly itemDialogSvc: ItemDialogService
+    private readonly itemDialogSvc: ItemDialogService,
+    private readonly authSvc: AuthService
   ) { }
 
   ngOnInit(): void {
+
+    this.isAuthenticated$ = this.authSvc.isAuthenticated$;
+
+    this._subsink.add(
+      this.authSvc.userId$.subscribe(id => this.userId = id)
+    );
   }
 
   ngOnChanges(changes) {
     this.itemDialogSvc.items.next(changes.items.currentValue);
+  }
+
+  ngOnDestroy() {
+    this._subsink.unsubscribe();
   }
 
   onLoadMore(event) {
