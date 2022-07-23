fcece072dee41831234805294a82cf03bd4f43cf
Marko Urh <urh.marko@gmail.com>
2022-07-23 23:02:27 +0200

    fix: preserve in cache

--- functions/src/services/currency.service.ts
+++ functions/src/services/currency.service.ts
@@ -1,3 +1,10 @@
+import moment = require("moment");
+
+interface EurRateCache {
+    eurRate: number;
+    time: Date;
+}
+
 export class CurrencyService {
     private readonly _store: FirebaseFirestore.Firestore
 
@@ -6,19 +13,41 @@ export class CurrencyService {
     }
 
     async getEurRate() {
+        return await this.fetchEurRateInternal();
+    }
+
+    private async fetchEurRateInternal() {
+        const eurRateCache = JSON.parse(localStorage.getItem('eurRate')) as EurRateCache;
+        if (!eurRateCache) {
+            return await this.getFromDb();
+        }
+
+        const start = moment(eurRateCache.time);
+        const end = moment(new Date());
+        const difference = moment.duration(end.diff(start));
+
+        if (difference.hours() >= 4) {
+            return await this.getFromDb();
+        }
+
+        return eurRateCache.eurRate;
+    }
+
+    private async getFromDb() {
         const doc = this._store.doc("config/global");
         const res = await doc.get();
         const dbVariables = res.data() as { eur: number };
+
+        // preserve in local cache
+        const cache = <EurRateCache>{ eurRate: dbVariables.eur, time: new Date() };
+        localStorage.setItem('eurRate', JSON.stringify(cache));
 
         return dbVariables.eur;
     }
 
     async formatHrkAndEur(hrk: number) {
-
         const eurRate = await this.getEurRate();
-
         const eur = Math.round(100 * hrk / eurRate) / 100;
-
         return `${hrk} kn | ${eur} €`
     }
 }
