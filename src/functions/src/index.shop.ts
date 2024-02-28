ad1be31f57a9e62158d7346f05cb6d400fb30b4f
Marko Urh <urh.marko@gmail.com>
2024-02-28 22:44:57 +0100

    no message

--- src/functions/src/index.shop.ts
+++ src/functions/src/index.shop.ts
@@ -1,8 +1,5 @@
 import { createCheckoutSessionFn } from "./functions/shop/create-checkout-session.function";
-import { setProductCatalogFn, setProductCatalogHttpFn } from "./functions/shop/set-product-catalog.function";
+import { setProductCatalogFn } from "./functions/shop/set-product-catalog.function";
 
 export const setProductCatalog = setProductCatalogFn;
 export const createCheckoutSession = createCheckoutSessionFn;
-
-// TODO: Remove
-export const setProductCatalogHttp = setProductCatalogHttpFn;
