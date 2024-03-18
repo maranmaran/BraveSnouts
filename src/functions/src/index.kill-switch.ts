36aec25d575120c160a1a947825be0b6202e3e5c
Marko Urh <urh.marko@gmail.com>
2024-03-18 23:13:11 +0100

    no message

--- src/functions/src/index.kill-switch.ts
+++ src/functions/src/index.kill-switch.ts
@@ -3,45 +3,69 @@ import { GoogleAuth } from "google-auth-library";
 import { google } from 'googleapis';
 import { appAdmin, appStore, europeFunctions } from "./functions/app";
 
+interface BudgetAlertData {
+    budgetDisplayName: string, // "Brave snouts price alert"
+    costAmount: number, // 0.16
+    costIntervalStart: Date, // "2024-03-01T08:00:00Z"
+    budgetAmount: number // 2.0,
+    budgetAmountType: string // "SPECIFIED_AMOUNT",
+    currencyCode: string // "USD"
+}
+
+interface ProjectBillingInfo {
+    /**
+     * The resource name of the billing account associated with the project, if any. For example, `billingAccounts/012345-567890-ABCDEF`.
+     */
+    billingAccountName?: string | null;
+    /**
+     * Output only. True if the project is associated with an open billing account, to which usage on the project is charged. False if the project is associated with a closed billing account, or no billing account at all, and therefore cannot use paid services.
+     */
+    billingEnabled?: boolean | null;
+    /**
+     * Output only. The resource name for the `ProjectBillingInfo`; has the form `projects/{project_id\}/billingInfo`. For example, the resource name for the billing information for project `tokyo-rain-123` would be `projects/tokyo-rain-123/billingInfo`.
+     */
+    name?: string | null;
+    /**
+     * Output only. The ID of the project that this `ProjectBillingInfo` represents, such as `tokyo-rain-123`. This is a convenience field so that you don't need to parse the `name` field to obtain a project ID.
+     */
+    projectId?: string | null;
+}
+
 export const killSwitch = europeFunctions().pubsub.topic('firebase-budget-alert')
     .onPublish(async (message, ctx) => {
-        logger.info(message);
-
         const PROJECT_ID = appAdmin().installations().app.options.projectId;
         const PROJECT_NAME = `projects/${PROJECT_ID}`;
+        if (!PROJECT_ID) {
+            throw new Error("No PROJECT_ID specified");
+        }
 
-        const data = JSON.parse(message.json);
-
-        logger.error('Budget exceeeded', { data });
+        const jsonRaw = Buffer.from(message.data, 'base64').toString();
+        const data = <BudgetAlertData>JSON.parse(jsonRaw);
 
         const dynamicBudget = (await appStore().doc('config/global').get()).data().budget;
 
-        logger.info(data);
-        logger.info(data.costAmount);
-        logger.info(data.budgetAmount);
-        logger.info('Dynamic budget', dynamicBudget);
+        logger.info('Alert data', data);
+        logger.info('Allowed budget limit', dynamicBudget);
 
-        return;
-        // if (data.costAmount <= dynamicBudget) {
-        //     logger.info("No action necessary.");
-        //     return `No action necessary. (Current cost: ${data.costAmount})`;
-        // }
+        if (data.costAmount <= dynamicBudget) {
+            logger.info("No action necessary.");
+            return `No action necessary. (Current cost: ${data.costAmount}, Allowed cost: ${dynamicBudget})`;
+        }
 
-        // if (!PROJECT_ID) {
-        //     logger.error("no project specified");
-        //     return 'No project specified';
-        // }
+        _setAuthCredential();
 
-        // _setAuthCredential();
+        logger.warn("Limit reached, shutting things down");
 
-        // const billingEnabled = await _isBillingEnabled(PROJECT_NAME);
-        // if (billingEnabled) {
-        //     logger.warn("disabling billing");
-        //     return _disableBillingForProject(PROJECT_NAME);
-        // } else {
-        //     logger.info("billing already disabled");
-        //     return 'Billing already disabled';
-        // }
+        const billingInfo = await getBillingInfo(PROJECT_ID, PROJECT_NAME);
+        const billingEnabled = billingInfo?.billingEnabled ?? true;
+
+        if (billingEnabled) {
+            logger.warn("disabling billing");
+            return await _disableBillingForProject(PROJECT_NAME, billingInfo);
+        } else {
+            logger.warn("Billing already disabled");
+            return 'Billing already disabled';
+        }
     })
 
 
@@ -67,22 +91,25 @@ const _setAuthCredential = () => {
  * @param {string} projectName Name of project to check if billing is enabled
  * @return {bool} Whether project has billing enabled or not
  */
-const _isBillingEnabled = async projectName => {
+const getBillingInfo = async (projectId, projectName) => {
     try {
         const billing = google.cloudbilling('v1').projects;
 
         const res = await billing.getBillingInfo({
-            name: projectName
+            name: projectName,
         });
 
-        console.info(res);
-
-        return res.data.billingEnabled;
+        return res.data;
     } catch (e) {
         console.error(
-            'Unable to determine if billing is enabled on specified project, assuming billing is enabled'
+            'Unable to get billing information on specified project, assuming billing is enabled'
         );
-        return true;
+        return <ProjectBillingInfo>{
+            projectId: projectId,
+            name: projectName,
+            billingEnabled: true,
+            billingAccountName: 'Brave Snouts'
+        };
     }
 };
 
@@ -91,16 +118,21 @@ const _isBillingEnabled = async projectName => {
  * @param {string} projectName Name of project disable billing on
  * @return {string} Text containing response from disabling billing
  */
-const _disableBillingForProject = async projectName => {
+const _disableBillingForProject = async (projectName: string, billingInfo: ProjectBillingInfo) => {
     const billing = google.cloudbilling('v1').projects;
 
+    // The billing account 
+    // https://console.cloud.google.com/billing/01FE42-36ED66-134A0D
+
+    // Disable billing
     const res = await billing.updateBillingInfo({
         name: projectName,
         requestBody: {
-            billingEnabled: false,
-            // https://console.cloud.google.com/billing/01FE42-36ED66-134A0D
-            billingAccountName: 'Brave Snouts'
-        }, // Disable billing
+            billingEnabled: false, // disables the billing
+            name: billingInfo?.name ?? null,
+            projectId: billingInfo?.projectId ?? null,
+            billingAccountName: billingInfo.billingAccountName ?? 'Brave Snouts',
+        },
     });
 
     console.info(res);
